c9aa2f65654a69e66e48afb386ef1d9a79525636
rammie90 <rammie90@gmail.com>
2026-07-21 00:03:38 +0900

    upload multiple files

--- script/upload_hugo_media.sh
+++ script/upload_hugo_media.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-
 set -e
 
 DEFAULT_DIR="$HOME/hugo/rammie-blog/static/images"
+TARGET_DIR="$DEFAULT_DIR"
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 <target_file> [target_dir]"
+usage() {
+    echo "Usage: $0 [-d target_dir] <file1> [file2 ...]"
     exit 1
-fi
+}
 
-FILE="$1"
-TARGET_DIR="${2:-$DEFAULT_DIR}"
+while getopts "d:" opt; do
+    case "$opt" in
+        d) TARGET_DIR="$OPTARG" ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND - 1))
 
-if [ ! -f "$FILE" ]; then
-    echo "Error: '$FILE' is not a file"
-    exit 1
+if [ "$#" -lt 1 ]; then
+    usage
 fi
 
 YYYY=$(date +%Y)
 MM=$(date +%m)
 DEST="$TARGET_DIR/$YYYY/$MM"
-
 mkdir -p "$DEST"
-mv "$FILE" "$DEST/"
-echo "Moved '$FILE' -> '$DEST/'"
+
+for FILE in "$@"; do
+    if [ ! -f "$FILE" ]; then
+        echo "Warning: '$FILE' is not a file, skipping" >&2
+        continue
+    fi
+    mv "$FILE" "$DEST/"
+    echo "Moved '$FILE' -> '$DEST/'"
+done
